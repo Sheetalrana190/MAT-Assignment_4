ccf42b4bf439a14c2c5eed875882a7016e139f07
sheetal <srana4226@conestoga.on.ca>
2025-04-09 15:27:27 -0400

    src/paintRequiredForMultipleCoats/paintRequiredForMultipleCoats.ts ; improvise the production code

--- src/paintRequiredForMultipleCoats/paintRequiredForMultipleCoats.ts
+++ src/paintRequiredForMultipleCoats/paintRequiredForMultipleCoats.ts
@@ -1,3 +1,6 @@
 export default function paintRequiredForMultipleCoats(area: number, coveragePerLiter: number, coats: number): number {
+  if (area <= 0 || coveragePerLiter <= 0 || coats <= 0) {
+    throw new Error("Area, coverage per liter, and coats must be positive and non-zero");
+  }
   return (area / coveragePerLiter) * coats;
 }
