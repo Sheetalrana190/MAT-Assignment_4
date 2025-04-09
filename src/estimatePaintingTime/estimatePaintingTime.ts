19078ad6854c2db8c47ace039b9cd4062b520849
sheetal <srana4226@conestoga.on.ca>
2025-04-09 15:19:49 -0400

    src/estimatePaintingTime/estimatePaintingTime.ts : Modify the production code as per the test cases

--- src/estimatePaintingTime/estimatePaintingTime.ts
+++ src/estimatePaintingTime/estimatePaintingTime.ts
@@ -1,3 +1,9 @@
 export default function estimatePaintingTime(area: number, paintingSpeed: number): number {
+  if (area < 0 || paintingSpeed < 0) {
+    throw new Error("Area and painting speed must be positive");
+  }
+  if (paintingSpeed === 0) {
+    throw new Error("Painting speed cannot be zero");
+  }
   return area / paintingSpeed;
 }
